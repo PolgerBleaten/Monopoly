6db4c5dc9fb126b48efcfbae545311c694ffb1fe
PolgerBleaten <71384763+PolgerBleaten@users.noreply.github.com>
2021-01-27 14:52:47 +0100

    Made the groundwork for the board

---
+++ main.js
@@ -0,0 +1,5 @@
+//math.random tärning, variabel för vart pjäserna är, hur mycket pengar som spelaren har, hur mycket pengar som banken har, vilka gator vilken spelare har
+rollDice();
+movePiece(player, numberofsteps);
+giveMoney();
+tryPay(player, money);
