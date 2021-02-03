b788f2cd8e570284b0271827cd6c7a38c08b06c2
PolgerBleaten <71384763+PolgerBleaten@users.noreply.github.com>
2021-02-03 14:56:41 +0100

    Added a basic dice function and changed some css

--- main.js
+++ main.js
@@ -1,5 +1,46 @@
 //math.random tärning, variabel för vart pjäserna är, hur mycket pengar som spelaren har, hur mycket pengar som banken har, vilka gator vilken spelare har
-rollDice();
-movePiece(player, numberofsteps);
-giveMoney();
-tryPay(player, money);
+let DiceButton = document.querySelector("[data-rolldicebutton]");
+DiceButton.addEventListener("click", rollDice);
+function rollDice(){
+    let dice = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("dice").innerHTML = dice;
+}
+function createPlayer() {
+    return{
+        piece,
+        money,
+        position,
+    };
+
+}
+//function 
+//movePiece(player, numberofsteps);
+//giveMoney();
+//tryPay(player, money);
+//var Game = function(){
+ /*var game = {};
+ game.tiles = [
+     new tile("Coco café", 50, ""), 
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+     new tile(),
+    ];
+}*/
