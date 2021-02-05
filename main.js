86171449336e2887417b9dfe06c4af37f4135732
PolgerBleaten <71384763+PolgerBleaten@users.noreply.github.com>
2021-02-05 15:03:39 +0100

    Added id:s to tiles, added a form for player info

--- main.js
+++ main.js
@@ -1,46 +1,26 @@
 //math.random tärning, variabel för vart pjäserna är, hur mycket pengar som spelaren har, hur mycket pengar som banken har, vilka gator vilken spelare har
 let DiceButton = document.querySelector("[data-rolldicebutton]");
+let playerForm = document.querySelector("[player]")
 DiceButton.addEventListener("click", rollDice);
 function rollDice(){
     let dice = Math.floor(Math.random() * 6) + 1;
     document.getElementById("dice").innerHTML = dice;
 }
-function createPlayer() {
-    return{
-        piece,
-        money,
-        position,
-    };
-
+playerForm.addEventListener("submit", (s)=> {
+    s.preventDefault();
+})
+function createPlayer1() {
+    var name = document.getElementById("name1");
+    var piece = document.getElementsByName("piece1");
+    var player = document.getElementById("player1");
+    for(i = 0; i < piece.length; i++) 
+    {
+        if(piece[i].checked)
+        document.getElementById("free").innerHTML = piece[i].value;
+    }
+    player.style.display = "none";
 }
 //function 
 //movePiece(player, numberofsteps);
 //giveMoney();
 //tryPay(player, money);
-//var Game = function(){
- /*var game = {};
- game.tiles = [
-     new tile("Coco café", 50, ""), 
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-     new tile(),
-    ];
-}*/
