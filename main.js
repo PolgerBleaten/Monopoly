161554a160aff104d3631a4ff8bf02614aa49fff
PolgerBleaten <71384763+PolgerBleaten@users.noreply.github.com>
2021-02-10 17:23:31 +0100

    Made minor changes

--- main.js
+++ main.js
@@ -1,24 +1,76 @@
 //math.random tärning, variabel för vart pjäserna är, hur mycket pengar som spelaren har, hur mycket pengar som banken har, vilka gator vilken spelare har
 let DiceButton = document.querySelector("[data-rolldicebutton]");
-let playerForm = document.querySelector("[player]")
+DiceButton.style.display = "none";
+let dice = 0;
+let ready = 0;
+document.querySelectorAll("[player]").forEach(item => {
+    item.addEventListener('submit', (s) => {
+        s.preventDefault();
+    })
+  })
+var Player1 = ["", 550, "", 0];
+var Player2 = ["", 550,"",0];
+let turn = 0;
 DiceButton.addEventListener("click", rollDice);
 function rollDice(){
-    let dice = Math.floor(Math.random() * 6) + 1;
+    dice = Math.floor(Math.random() * 6) + 1;
     document.getElementById("dice").innerHTML = dice;
-}
-playerForm.addEventListener("submit", (s)=> {
-    s.preventDefault();
-})
-function createPlayer1() {
-    var name = document.getElementById("name1");
-    var piece = document.getElementsByName("piece1");
-    var player = document.getElementById("player1");
-    for(i = 0; i < piece.length; i++) 
-    {
-        if(piece[i].checked)
-        document.getElementById("free").innerHTML = piece[i].value;
+    turn++;
+    document.getElementById("round").innerHTML = turn;
+    if (turn%2 == 1){
+        Player1[3] += dice;
     }
-    player.style.display = "none";
+    else{
+        Player2[3] += dice;
+    }
+    if (Player2[3] >= 40 && !Player1[3] >= 40){
+        Player2[3] -= 40;
+    }
+    else if(Player1[3] >= 40 && !Player2[3] >= 40){
+        Player1[3] -= 40;
+    }
+    else if(Player1[3] >= 40 && Player2[3] >= 40){
+        Player1[3] -= 40;
+        Player2[3] -= 40;
+    }
+    document.getElementById("Steps1").innerHTML = Player1[3];
+    document.getElementById("Steps2").innerHTML = Player2[3];
+}
+
+function createPlayer(person){
+    if (person == 1){
+        Player1[0] = document.getElementById("name1").value;
+        var piece = document.getElementsByName("piece1");
+        var player = document.getElementById("player1");
+        for(i = 0; i < piece.length; i++)
+        {
+            if(piece[i].checked)
+            Player1[2] = piece[i].value;
+        }
+        player.style.display = "none";
+        document.getElementById("Name1").innerHTML = "Spelare 1: " + Player1[0]
+        document.getElementById("Piece1").innerHTML = Player1[2];
+        document.getElementById("Money1").innerHTML = Player1[1] + " mk";
+        ready++;
+    }
+    else{
+        Player2[0] = document.getElementById("name2").value;
+        var piece = document.getElementsByName("piece2");
+        var player = document.getElementById("player2");
+        for(i = 0; i < piece.length; i++)
+        {
+            if(piece[i].checked)
+            Player2[2] = piece[i].value;
+        }
+        player2.style.display = "none";
+        document.getElementById("Name2").innerHTML = "Spelare 2: " + Player2[0]
+        document.getElementById("Piece2").innerHTML = Player2[2];
+        document.getElementById("Money2").innerHTML = Player2[1] + " mk";
+        ready++;
+    }
+    if (ready == 2){
+        DiceButton.style.display = "initial";
+    }
 }
 //function 
 //movePiece(player, numberofsteps);
