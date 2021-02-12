48fa6b1585dcf2a3ee1d0261269727c7cef592a2
PolgerBleaten <71384763+PolgerBleaten@users.noreply.github.com>
2021-02-12 17:38:38 +0100

    Added a function that lets you pay for streets

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 //math.random tärning, variabel för vart pjäserna är, hur mycket pengar som spelaren har, hur mycket pengar som banken har, vilka gator vilken spelare har
 let DiceButton = document.querySelector("[data-rolldicebutton]");
+let BuyButton = document.querySelector("[data-buystreetbutton]");
+let NotBuyButton = document.querySelector("[data-donotbuystreetbutton]");
 DiceButton.style.display = "none";
+BuyButton.style.display = "none";
+NotBuyButton.style.display = "none";
 let dice = 0;
 let ready = 0;
 document.querySelectorAll("[player]").forEach(item => {
@@ -11,6 +15,7 @@ document.querySelectorAll("[player]").forEach(item => {
 var Player1 = ["", 550, "", 0];
 var Player2 = ["", 550, "", 0];
 let turn = 0;
+BuyButton.addEventListener("click", tryPay);
 DiceButton.addEventListener("click", rollDice);
 function rollDice(){
     dice = Math.floor(Math.random() * 6) + 1;
@@ -35,6 +40,7 @@ function rollDice(){
     }
     document.getElementById("Steps1").innerHTML = Player1[3];
     document.getElementById("Steps2").innerHTML = Player2[3];
+    buyStreet(turn, Player1[3], Player2[3]);
 }
 
 function createPlayer(person){
@@ -72,23 +78,66 @@ function createPlayer(person){
         DiceButton.style.display = "initial";
     }
 }
-function buyStreet(turn, tile){
-    if (tile%10 == 0 || tile == 3 || tile == 5 || tile == 12 || tile == 17 || tile == 19 || tile == 23 || tile == 28 || tile == 32 || tile == 36 || tile == 38)
+function buyStreet(turn, tile1, tile2){
+    if (turn %2 == 1){
+        tile = tile1;
+    }
+    else{
+        tile = tile2;
+    }
+    if (tile%10 == 0)
+    {
+        document.getElementById("tilevalue").innerHTML = "Inget";
+    }
+    else if(tile == 5 || tile == 12 || tile == 28 || tile == 32)
     {
 
     }
-    else{
-        if(tile > 12){
-            var pricetile = document.querySelectorAll(".pay");
-            for(var i=0; i < pricetile.length; i++){
-                if(pricetile[i].length() > 5){
-                    
-                }
-            }
+    else if(tile == 19 || tile == 38)
+    {
+        document.getElementById("playerinfo").innerHTML = "Matkorten gick ut! Du förlorade 100 matkort.";
+        if (turn%2 == 1)
+        {
+            Player1[1] -=100;
         }
+        else{
+            Player2[1] -=100;
+        }
+    }
+    else if(tile == 3 || tile == 17 || tile == 23 || tile == 36)
+    {
+        
+    }
+    else{
+        var streetname = document.getElementById("n" + tile).innerHTML;
+        if(tile < 12){
+            var pricetext = document.getElementById("t" + tile).innerHTML;
+            var price = pricetext.substring(0,2);
+        }
+        else{
+            var pricetext = document.getElementById("t" + tile).innerHTML;
+            var price = pricetext.substring(0,3);
+        }
+        document.getElementById("playerinfo").innerHTML = "Vill du köpa " + streetname + " för " + price + " matkort?";
     }
 }
 //function 
-//movePiece(player, numberofsteps);
 //giveMoney();
-//tryPay(player, money);
+function tryPay(player, price, tile)
+{
+    var streetname = document.getElementById("n" + tile).innerHTML;
+    if (player == 1) {
+        var money = Player1[1];
+    }
+    else{
+        var money = Player2[1]; 
+    }
+    if (price > money){
+        document.getElementById("playerinfo").innerHTML = "Du har inte nog med pengar för att köpa " + streetname +"!";
+    }
+    else{
+        money -= price;
+        document.getElementById("playerinfo").innerHTML = "Du köpte " + streetname + "! Du har nu " + money + " matkort kvar.";
+        document.getElementById("n" + tile).style.borderTop = "5px solid #0000FF";
+    }
+}
